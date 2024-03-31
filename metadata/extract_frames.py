e15c61c57125349ceb30062bfbab80b85e2006f6
ly245422 <140572108+ly245422@users.noreply.github.com>
2024-03-31 14:49:21 +0800

    Update extract_frames.py

--- metadata/extract_frames.py
+++ metadata/extract_frames.py
@@ -1,20 +1,21 @@
 ## Extract frames using ffmpeg and parallel
 import subprocess
 
-dir_video_new_save_root = r"/ChaoticWorld/videos_320_180" # Specify your own original video path
-
+dir_video_org = r"/ChaoticWorld/videos" # Specify your own original video path
+dir_video_new_save_root = r"/ChaoticWorld/frames_320_180" # Specify your own frame path
 
 print('Extracting videos')
 
 cmd = '''
 num_threads=20
-src_path=''' + dir_video_new_save_root + '''
-dst_path=''' + dir_video_new_save_root.replace('videos', 'frames') + '''
+src_path=''' + dir_video_org + '''
+dst_path=''' + dir_video_new_save_root + '''
 
 mkdir $dst_path
 
-parallel -j $num_threads "mkdir ${dst_path}/{};ffmpeg -i ${src_path}/{}.mp4 -start_number 1 ${dst_path}/{}/'{}_%06d.png' -loglevel error" ::: `ls ${src_path} |cut -d '.' -f1`
+parallel -j $num_threads "mkdir ${dst_path}/{};ffmpeg -i ${src_path}/{}.mp4 -vf scale=320:180 -start_number 1 ${dst_path}/{}/'{}_%06d.png' -loglevel error" ::: `ls ${src_path} | cut -d '.' -f1`
 '''
+
 subprocess.check_output(cmd, shell=True) # frames are 1-indexed
 
 print('Extracting videos completed')
